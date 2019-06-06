350ed0b290bb6deffc2f32bca60033b8a80e205a
ManojSilag <40380004+ManojSilag@users.noreply.github.com>
2019-06-06 12:55:24 +0530

    Dynamic caching

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-    const staticCacheName = 'site-static';
+    const staticCache = 'site-static-v2';
+    const dynamicCache = 'site-dynamic-v1';
     const assets = [
     '/',
     '/index.html',
@@ -17,7 +18,7 @@
     self.addEventListener('install', evt => {
     //console.log('service worker installed');
     evt.waitUntil(
-        caches.open(staticCacheName).then((cache) => {
+        caches.open(staticCache).then((cache) => {
         console.log('caching shell assets');
         cache.addAll(assets);
         })
@@ -35,7 +36,13 @@
     evt.respondWith(
         caches.match(evt.request)
             .then(cacheRes => {
-                return cacheRes || fetch(evt.request);
+                return cacheRes || fetch(evt.request)
+                .then(fetchRes => {
+                    return caches.open(dynamicCache).then(cache => {
+                        cache .put(evt.request.url, fetchRes.clone());
+                        return fetchRes;
+                    })
+                });
             })
-    )
+    );
     });
